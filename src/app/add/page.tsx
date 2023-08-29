3974280421a0cd590a28c71b4a136b285b7dacd0
Hanne Maren Helgedagsrud <hhelgedagsrud@SopraSteriaBiometrics.onmicrosoft.com>
2023-08-29 14:48:49 +0200

    Add content to navigation bar. User can click on "Add" and "Graph" to navigate to different pages.

---
+++ src/app/add/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Add = () => {
+
+    return (<div> Add temperature </div>)
+}
+
+export default Add;
--- src/app/components/navigation/Navbar.tsx
+++ src/app/components/navigation/Navbar.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
     return (
-        <div>
-            MyNavbar
+        <div className="w-full sticky top-0 mx-auto">
+            <ul className="flex justify-center flex-row gap-x-6 py-4 h-full">
+                <li>
+                    <Link href="/add">
+                        <p>Add</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/graph">
+                        <p>Graph</p>
+                    </Link>
+                </li>
+            </ul>
         </div>)
 }
 
