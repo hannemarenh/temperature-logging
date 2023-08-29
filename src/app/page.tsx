e4b762520a8a5ecc890ce97a0ffb42da4b3d0781
Hanne Maren Helgedagsrud <hhelgedagsrud@SopraSteriaBiometrics.onmicrosoft.com>
2023-08-29 14:59:57 +0200

    Add image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-between p-24">
-          Hello
-
-    </main>
-  )
+    const imageStyle = {
+        borderRadius: '40%'
+    }
+    return (
+        <main className="flex flex-col items-center justify-between ">
+            <Image src={"/unicorn.png"} height={500} width={500} style={imageStyle} alt={'Unicorn'} />
+        </main>
+    )
 }
